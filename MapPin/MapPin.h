1a8af34186d8cf464ed902b19497974789e8255c
Hiral Naik <hrlnaik@gmail.com>
2018-12-18 10:52:43 -0800

    Initial Commit

---
+++ MapPin/MapPin.h
@@ -0,0 +1,19 @@
+//
+//  MapPin.h
+//  MapPin
+//
+//  Created by Hiral Naik on 12/18/18.
+//  Copyright © 2018 Hiral Naik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MapPin.
+FOUNDATION_EXPORT double MapPinVersionNumber;
+
+//! Project version string for MapPin.
+FOUNDATION_EXPORT const unsigned char MapPinVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapPin/PublicHeader.h>
+
+
